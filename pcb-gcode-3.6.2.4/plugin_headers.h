4ea2b09a1a9ca2c57b3fb95a03f98d0333abae1b
Nathan Cheek <nathan@nathancheek.com>
2017-12-17 21:21:11 -0500

    Cleanup 2018 repository, removing files archived in 2017 repository

--- pcb-gcode-3.6.2.4/plugin_headers.h
+++
@@ -1,5 +0,0 @@
-#include "source/nonvolatile.h"
-
-#include "plugins/settings.plugin.h"
-#include "plugins/calculator.plugin.h"
- 
--- pcb-gcode-3.6.2.4/source/viewer.linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-PATH=/usr/local/jdk/bin:$PATH
-
-cd "`dirname $0`/../viewer/application.linux"
-./viewer >viewer.log 2>&1
